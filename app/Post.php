3257add89c78efbd7aa5f01971d38b1e04c297dd
Maria Melisa H <mmelisaher@gmail.com>
2024-06-09 11:09:35 +0700

    ngontrol postan web

--- app/Post.php
+++ app/Post.php
@@ -1,1 +1,22 @@
-// bikin function untuk ngepost disini
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $guarded = [
+        'id', 'flag', 'created_at', 'updated_at',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comment()
+    {
+        return $this->hasMany(Comment::class)->orderBy('updated_at', 'DESC');
+    }
+}
