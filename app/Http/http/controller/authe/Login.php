13f440f4710c8a5787964d23bf03fef76b361127
monicajala <monicaho1106@gmail.com>
2024-06-11 10:45:44 +0700

    progres2.2

--- app/Http/http/controller/authe/Login.php
+++ app/Http/http/controller/authe/Login.php
@@ -1,3 +1,29 @@
+<?php
+session_start();
+
+if (isset($_POST['submit'])) {
+    include("php/config.php");
+
+    $email = mysqli_real_escape_string($con, $_POST['email']);
+    $password = mysqli_real_escape_string($con, $_POST['password']);
+
+    $result = mysqli_query($con, "SELECT * FROM users WHERE Email='$email' AND Password='$password'") or die("Select Error");
+    $row = mysqli_fetch_assoc($result);
+
+    if (is_array($row) && !empty($row)) {
+        $_SESSION['valid'] = $row['Email'];
+        $_SESSION['username'] = $row['Username'];
+        $_SESSION['id'] = $row['Id'];
+        header("Location: home.php");
+        exit();
+    } else {
+        echo "<div class='message'>
+        <p>Wrong Username or Password</p>
+        </div> <br>";
+        echo "<a href='Login.php'><button class='btn'>Go Back</button></a>";
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,26 +39,23 @@
             <header>Login</header>
             <form action="" method="post">
                 <div class="field input">
-                    <label for="username">username</label>
-                    <input type="text" name="username" id="username" required>
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email" required>
                 </div>
 
                 <div class="field input">
-                    <label for="password">password</label>
+                    <label for="password">Password</label>
                     <input type="password" name="password" id="password" required>
                 </div>
 
                 <div class="field">
-                    
                     <input type="submit" name="submit" value="Login" required>
                 </div>
                 <div class="links">
                     Don't have account? <a href="SignUp.php">Sign Up Now</a>
                 </div>
-
             </form>
         </div>
     </div>
-    
 </body>
 </html>
