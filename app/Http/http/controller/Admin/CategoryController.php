8a9dbd99c175611204d0cd1a320347453e88eb74
monicajala <monicaho1106@gmail.com>
2024-06-18 13:39:23 +0700

    progres1.1

---
+++ app/Http/http/controller/Admin/CategoryController.php
@@ -0,0 +1,44 @@
+<?php 
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Category;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use App\Http\Middleware\IsAdmin;
+use App\Http\Controllers\Controller;
+use App\Http\Middleware\Authenticate;
+
+class CategoryController extends Controller
+{
+    public function __construct()
+    {
+        return $this->middleware([IsAdmin::class, Authenticate::class]);
+    }
+
+    public function index()
+    {
+        return view('admin.categories.index', [
+            'categories'    => Category::all(),
+        ]);
+    }
+
+    public function create()
+    {
+        return view('admin.categories.create');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => ['required', 'unique:categories'],
+        ]);
+
+        Category::create([
+            'name' => $request->name,
+            'slug' => Str::slug($request->name),
+        ]);
+
+        return redirect()->route('admin.categories.index')->with('success', 'Category Created');
+    }
+}
