87379f0a60dd3dafefc6afd68b3c608c368bf2cf
amnnic <amnnic07@gmail.com>
2024-06-20 21:03:09 +0700

    progress 12.1 feature

---
+++ tests/Feature/ProfileInformationTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Jetstream\Http\Livewire\UpdateProfileInformationForm;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class ProfileInformationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_current_profile_information_is_available()
+    {
+        $this->actingAs($user = User::factory()->create());
+
+        $component = Livewire::test(UpdateProfileInformationForm::class);
+
+        $this->assertEquals($user->name, $component->state['name']);
+        $this->assertEquals($user->email, $component->state['email']);
+    }
+}
