c513f74751eaefe946088a9851362e208a73733e
monicajala <monicawork3006@gmail.com>
2024-06-22 20:10:32 +0700

    4.2.1

--- app/Jobs/CreateThread.php
+++ app/Jobs/CreateThread.php
@@ -31,5 +31,36 @@ class CreatedThread
         $this->tags = $tags;
         $this->author = $author;
     }
-    
+
+    public static function fromRequest(ThreadStoreRequest $request): self
+    {
+        return new static(
+            $request->title(),
+            $request->body(),
+            $request->category(),
+            $request->tags(),
+            $request->author(),
+        );
+    }
+    /**
+     * Execute the job.
+     * @return void
+     */
+    public function handle(): Thread
+    {
+        $thread = new Thread([
+            'title'         => $this->title,
+            'slug'          => Str::slug($this->title),
+            'body'          => Purifier::clean($this->body),
+            'category_id'   => $this->category,
+        ]);
+
+        $thread->authorBy($this->author);
+        $thread->syncTags($this->tags);
+        $thread->save();
+
+        event(new ThreadWasCreated($thread));
+
+        return $thread;
+    }
 }
