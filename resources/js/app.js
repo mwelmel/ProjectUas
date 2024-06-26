422355c4ba3dfce7d622bd362da0a0bdcc53065b
Maria Melisa H <mmelisaher@gmail.com>
2024-06-26 19:47:10 +0700

    perubahan pada JS

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
-import './bootstrap';
+require('./bootstrap');
+
+import Alpine from 'alpinejs'
+
+window.Alpine = Alpine
+
+Alpine.start()
+
+import Choices from 'choices.js';
+
+// Create multiselect element
+window.choices = (element) => {
+    return new Choices(element, {
+        maxItemCount: 3,
+        removeItemButton: true,
+    });
+}
+
